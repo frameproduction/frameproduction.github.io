f687573ed90ffff97a6f955869a9a40187729acf
frameproduction <135406982+frameproduction@users.noreply.github.com>
2023-07-10 12:07:48 +0700

    update very first demo

---
+++ app.js
@@ -0,0 +1,20 @@
+$(document).ready(function () {
+    if (window.pageYOffset > 50) {
+        $('header').addClass('minimize');
+        $('.negative-1').addClass('minimize');
+    } else {
+        $('header').removeClass('minimize');
+        $('.negative-1').removeClass('minimize');
+    }
+
+    $(window).scroll(function () { 
+        if (window.pageYOffset > 50) {
+            $('header').addClass('minimize');
+            $('.negative-1').addClass('minimize');
+        } else {
+            $('header').removeClass('minimize');
+            $('.negative-1').removeClass('minimize');
+        }
+    });
+    
+});
